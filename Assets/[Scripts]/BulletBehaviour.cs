abd83e8a082a41120b17de6ef5143ac4e14b0f6d
abdulhakeemidr <56049093+abdulhakeemidr@users.noreply.github.com>
2021-10-12 14:03:12 -0400

    Created Player Bullet and Bullet Direction

--- Assets/[Scripts]/BulletBehaviour.cs
+++ Assets/[Scripts]/BulletBehaviour.cs
@@ -8,12 +8,30 @@ public class BulletBehaviour : MonoBehaviour
     [Range(0.0f, 0.5f)]
     public float speed;
     public Bounds bulletBounds;
+    public BulletDirection bulletDirection;
 
     private BulletManager bulletManager;
+    private Vector3 velocity;
 
     void Start()
     {
         bulletManager = GameObject.FindObjectOfType<BulletManager>();
+
+        switch(bulletDirection)
+        {
+            case BulletDirection.UP:
+                velocity = new Vector3(0.0f, speed, 0.0f);
+                break;
+            case BulletDirection.RIGHT:
+                velocity = new Vector3(speed, 0.0f, 0.0f);
+                break;
+            case BulletDirection.DOWN:
+                velocity = new Vector3(0.0f, -speed, 0.0f);
+                break;
+            case BulletDirection.LEFT:
+                velocity = new Vector3(-speed, 0.0f, 0.0f);
+                break;
+        }
     }
 
     void FixedUpdate()
@@ -24,15 +42,22 @@ public class BulletBehaviour : MonoBehaviour
 
     private void Move()
     {
-        transform.position -= new Vector3(0.0f, speed, 0.0f);
+
+        transform.position += velocity;
     }
 
     private void CheckBounds()
     {
+        // checks bottom bounds
         if(transform.position.y < bulletBounds.max)
         {
-            //Destroy(this.gameObject);
+            // returns the bullet to the cue when it leaves the screen
+            bulletManager.ReturnBullet(this.gameObject);
+        }
 
+        // checks top bounds
+        if (transform.position.y > bulletBounds.min)
+        {
             // returns the bullet to the cue when it leaves the screen
             bulletManager.ReturnBullet(this.gameObject);
         }
---
+++ Assets/[Scripts]/BulletDirection.cs
@@ -0,0 +1,11 @@
+using System.Collections;
+using System.Collections.Generic;
+
+[System.Serializable]
+public enum BulletDirection
+{
+    UP,
+    RIGHT,
+    DOWN,
+    LEFT
+}
